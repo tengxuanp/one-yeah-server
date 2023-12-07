496c613ef9783f8c7364225e7b008c146dcd8cb1
Raphael <raphael@alinehrms.com>
2023-12-07 23:28:38 +0800

    alpha-1.0

--- index.js
+++ index.js
@@ -41,12 +41,13 @@ io.on("connection",(socket)=>{
           logToClient(roomID, `Player ${username} joined room ${roomID}.`)
 
           const existingUsernames = getUsernamesInRoom(roomID);
-          setUsernamesInRoom(roomID,[...existingUsernames, username])
+          // setUsernamesInRoom(roomID,[...existingUsernames, username])
+          setUserInRoom(roomID, username, socket.id);
+
           socket.currentUser.host = false;
 
           // Emit the updated userlist to all clients in the room
           io.to(roomID).emit('usersRoomReceive', getUsernamesInRoom(roomID));
-
 
         } else {
           const newRoomID = generateUniqueCode(); // Generate a unique room ID
@@ -57,24 +58,22 @@ io.on("connection",(socket)=>{
           logToClient(newRoomID, `Player ${username} created room ${newRoomID}.`)
           socket.currentUser.host = true;
 
-          setUsernamesInRoom(newRoomID,[username])
+          // setUsernamesInRoom(newRoomID,[username])
+          // setUsernamesToSocketIDs(socket.id,socket.currentUser.username)
+          setUserInRoom(newRoomID, username, socket.id);
+
         }
       });
       
     socket.on("usersRoomRequest", (data) => { 
       let usernames = getUsernamesInRoom(data)
-      console.log('usernames in room:')
-      console.log(usernames)
+
+      let socketIDs = usernames.map(username => getSocketIDByUsername(username));
+    
 
       if(data){
         socket.emit('usersRoomReceive',usernames)
         checkHost(socket)
-        // if(socket.currentUser.host === true){
-        //   socket.emit('checkHost',{host:true});
-        // }else{
-        //   socket.emit('checkHost',{host:false});
-        // }
-
       }else{
         console.log(`Room ${data} does not exist.`);
       }
@@ -84,36 +83,108 @@ io.on("connection",(socket)=>{
       io.to(socket.currentUser.roomID).emit("game_initiated",gameid);
     })
 
+    socket.on("request_start_game_session", ({roomid, gameid})=>{
+      io.to(roomid).emit("start_game_session",({roomid,gameid}));
+      console.log('server2')
+    })
 
-    socket.on("start_game_session", async ({ roomid, gameid }) => {
-    io.to(roomid).emit("game_started");
-    if(gameid == 1){ //GAME 1      
+    socket.on("game_session", async ({ roomid, gameid }) => {
+      console.log('server4')
 
-      let currentQuestion = null;
-    
-      try {
-        // Get the current question
-        currentQuestion = await generateNewQuestion();
-    
-        if (currentQuestion) {
-          // Broadcast the current question to all users in the room
-          io.to(roomid).emit('current_question', currentQuestion);
-        } else {
-          console.error('Failed to get a new question');
-        }
-      } catch (error) {
-        console.error('Error generating a new question:', error);
+      socket.currentUser.gameState = {
+        gameStarted: false,
+        users: getUsernamesInRoom(roomid),
+        currentPlayerIndex: null,
+        scores: {},
+      };
+
+      const {gameState} = socket.currentUser
+
+      io.to(roomid).emit("game_started",gameState);
+      //GAME 1
+      if(gameid == 1){ 
+        
+        let currentQuestion = null;
+
+        //DECIDE WHOSE TURN
+        decideTurn({gameState})
+
+        //QUESTION
+        generateQuestion({currentQuestion,roomid})
+
+
+        socket.on('handleAnswer', ({ socketid, answer, roomid }) => {
+          let username;
+        
+          gameState.users.forEach(user => {
+            if (getSocketIDByUsername(user) === socketid) {
+              username = user;
+            }
+          });
+        
+          const updatedScores = {
+            ...gameState.scores,
+            [username]: (gameState.scores[username] || 0) + answer,
+          };
+        
+          gameState.scores = updatedScores;
+        
+          // Emit the updated scores to all clients in the room
+          io.to(roomid).emit('updated_scores', gameState.scores);
+        
+          if (updatedScores[username] > 4) {
+            let winner = username;
+            io.to(roomid).emit('game_over', { winner });
+          } else {
+            decideTurn({ gameState });
+            let currentQuestion = null;
+            generateQuestion({ currentQuestion, roomid });
+          }
+        });
+
+        socket.on('clear_scores', ({ roomid }) => {
+          gameState.scores = {}
+          const room = io.sockets.adapter.rooms.get(roomid);
+          if (room) {
+            for (const socketId of room) {
+              const socketInRoom = io.sockets.sockets.get(socketId);
+              if (socketInRoom && socketInRoom.currentUser) {
+                socketInRoom.currentUser.gameState.scores = {};
+              }
+            }
+          }
+          io.to(roomid).emit('game_restarted');
+          io.to(roomid).emit('updated_scores', gameState.scores);
+          decideTurn({ gameState });
+          let currentQuestion = null;
+          generateQuestion({ currentQuestion, roomid });
+        });
+
+        console.log(gameState)
+
+        socket.on('disconnect_all_users', () => {
+          const roomID = socket.currentUser.roomID;
+          io.to(roomID).emit('disconnect_all_users');
+          socket.disconnect(true);
+        });
+
+        socket.on('disconnect_user', () => {
+          socket.emit('disconnect_user')
+          socket.disconnect(true);
+        });
+
       }
-    }
-
-
+      
     });
+
+    
   
 })
 
 server.listen(3001, () => {
     console.log('SERVER IS RUNNING');
 })
+
 
 function checkHost(socket) {
   const isHost = socket.currentUser && socket.currentUser.host === true;
@@ -122,47 +193,89 @@ function checkHost(socket) {
 }
 
 function generateUniqueCode() {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let code = '';
-    for (let i = 0; i < 6; i++) {
-      const randomIndex = Math.floor(Math.random() * characters.length);
-      code += characters[randomIndex];
-    }
-    return code;
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let code = '';
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    code += characters[randomIndex];
   }
+  return code;
+}
 
-  const generateNewQuestion = async () => {
-    try {
-      const response = await fetch('https://api.truthordarebot.xyz/v1/truth');
-      const data = await response.json();
-      const {question} = data
+// const fetch = require('node-fetch');
+const fetchQuestionFromAPI = async () => {
+  try {
+    const response = await fetch('https://api.truthordarebot.xyz/v1/truth');
+    const data = await response.json();
+    const {question} = data
+
+    return {
+      // category: data.category,
+      // question: data.truth,
+      question
+    };
+  } catch (error) {
+    console.error('Error fetching truth data:', error);
+    return null; // Return null if there's an error
+  }
+};
+
+function decideTurn({gameState}) {
+  gameState.gameStarted = true;
+
+  gameState.currentPlayerIndex = Math.floor(Math.random() * gameState.users.length)
+
+  let ChosenPlayer = gameState.users[gameState.currentPlayerIndex];
+  // let currentPlayerSocketID = getSocketIDByUsername(ChosenPlayer)
+
+  // io.to(currentPlayerSocketID).emit("current_player_turn");
+  gameState.users.forEach((user, index) => {
+    const currentPlayerSocketID = getSocketIDByUsername(user);
+    const isCurrentPlayer = index === gameState.currentPlayerIndex;
+
+    io.to(currentPlayerSocketID).emit("current_player_turn", isCurrentPlayer);
+  });
   
-      return {
-        // category: data.category,
-        // question: data.truth,
-        question
-      };
-    } catch (error) {
-      console.error('Error fetching truth data:', error);
-      return null; // Return null if there's an error
-    }
-  };
+}
 
-  // Define a Map to store usernames in rooms
+const generateQuestion = async ({currentQuestion,roomid}) => {
+  try {
+    // Get the current question
+    currentQuestion = await fetchQuestionFromAPI();
+    if (currentQuestion) {
+      // Broadcast the current question to all users in the room
+      io.to(roomid).emit('current_question', currentQuestion);
+    } else {
+      console.error('Failed to get a new question');
+    }
+  } catch (error) {
+    console.error('Error generating a new question:', error);
+  }
+}
+
+// Define a Map to store usernames in rooms
 const usernamesInRooms = new Map();
+
+const usernamesToSocketIDs = new Map();
 
 // Function to get usernames in a specific room
 function getUsernamesInRoom(roomID) {
   return usernamesInRooms.get(roomID) || [];
 }
 
-// Function to set usernames in a specific room
-function setUsernamesInRoom(roomID, usernames) {
-  usernamesInRooms.set(roomID, usernames);
+function getSocketIDByUsername(username) {
+  return usernamesToSocketIDs.get(username);
 }
+
+function setUserInRoom(roomID, username, socketID) {
+  const usernames = usernamesInRooms.get(roomID) || [];
+  usernamesInRooms.set(roomID, [...usernames, username]);
+  usernamesToSocketIDs.set(username, socketID);
+}
+
 
 function logToClient(roomID, message){
   console.log('server sends log')
-  // io.emit('log_message',{message})
   io.to(roomID).emit('log_message', { message });
 }
+
