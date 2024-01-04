191b27786542647142e3a45066e32a61017b2c61
Raphael <raphael@alinehrms.com>
2024-01-04 15:18:00 +0800

    Test userlist

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ io.on("connection",(socket)=>{
     // Set Username
     socket.on('set_username', (data) => {
         socket.currentUser.username = data.username
-        console.log('Current User Details:', socket.currentUser);
+        console.log('Current User Details 1:', socket.currentUser);
         console.log(`Player ${socket.currentUser.username} created.`)
         // Check if roomID is available before logging to the client
         if (socket.currentUser.roomID) {
@@ -40,7 +40,7 @@ io.on("connection",(socket)=>{
 
     // Create or Join room
     socket.on("joinRoom", (roomID) => {
-
+      console.log('Current User Details 2:', socket.currentUser);
       let username = socket.currentUser.username
 
         if (roomID) {
